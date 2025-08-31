3d2b2ac86214112d45daf7a801a193f0d04f6dd3
BZzzzi <khj23465@naver.com>
2025-08-31 18:23:01 +0900

    Chore: 주석 삭제

--- src/hooks/useMe.ts
+++ src/hooks/useMe.ts
@@ -1,4 +1,3 @@
-// hooks/useMe.ts
 import { useQuery } from "@tanstack/react-query";
 import { fetchKakaoMe } from "@/lib/api/kakao.ts";
 
@@ -6,8 +5,6 @@ export function useMe() {
   return useQuery({
     queryKey: ["me"],
     queryFn: async () => {
-      // 토큰이 없으면 fetchKakaoMe 내부에서 Kakao.Auth 준비 중 에러날 수 있음
-      // 필요하면 토큰 체크를 선행해도 됩니다.
       return await fetchKakaoMe();
     },
     retry: 0,
