1985ed5f3ce8671e99dde198d09451d86c1f3c57
Jaeyeon-23 <reenact115@gmail.com>
2025-09-01 23:13:00 +0900

    Feat: 챌린지 아이콘 컴포넌트 추가

---
+++ src/components/Icon/ChallengeIcon.tsx
@@ -0,0 +1,55 @@
+import BallBlack from "@/assets/ball-black.svg";
+import BallColor from "@/assets/ball-color.svg";
+import BookBlack from "@/assets/book-black.svg";
+import BookColor from "@/assets/book-color.svg";
+import BroomBlack from "@/assets/broom-black.svg";
+import BroomColor from "@/assets/broom-color.svg";
+import BusBlack from "@/assets/bus-black.svg";
+import BusColor from "@/assets/bus-color.svg";
+import EditBlack from "@/assets/edit-black.svg";
+import EditColor from "@/assets/edit-color.svg";
+import WaterBlack from "@/assets/water-black.svg";
+import WaterColor from "@/assets/water-color.svg";
+
+type IconName = "ball" | "book" | "broom" | "bus" | "edit" | "water";
+type IconVariant = "black" | "color";
+
+const ICONS: Record<IconName, Record<IconVariant, string>> = {
+  ball: { black: BallBlack, color: BallColor },
+  book: { black: BookBlack, color: BookColor },
+  broom: { black: BroomBlack, color: BroomColor },
+  bus: { black: BusBlack, color: BusColor },
+  edit: { black: EditBlack, color: EditColor },
+  water: { black: WaterBlack, color: WaterColor },
+};
+
+interface ChallengeIconProps {
+  name: IconName;
+  variant?: IconVariant;
+  size?: number; // px 기본 24
+  className?: string; // 간격 패딩 필요할경우
+  alt?: string;
+}
+
+function ChallengeIcon({
+  name,
+  variant = "black",
+  size = 24,
+  className,
+  alt = "",
+}: ChallengeIconProps) {
+  const src = ICONS[name][variant];
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      className={className}
+      style={{ width: size, height: size }}
+      loading="lazy"
+      decoding="async"
+    />
+  );
+}
+
+export default ChallengeIcon;
--- src/pages/Collection.tsx
+++ src/pages/Collection.tsx
@@ -1,10 +1,17 @@
 import TopNavBar from "@/components/Navbar/TopNavBar";
 import BottomNavBar from "@/components/Navbar/BottomNavBar";
+import Icon from "@/components/Icon/ChallengeIcon";
 
 export default function Collection() {
   return (
     <>
       <TopNavBar title="컬렉션" />
+
+      {/* 아이콘 테스트용 */}
+      <div className="p-4">
+        <Icon name="book" variant="color" size={40} alt="책 아이콘" />
+      </div>
+
       <BottomNavBar />
     </>
   );
